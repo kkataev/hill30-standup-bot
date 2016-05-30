d17b9f399f7af42c548c17be162ecb1bb4f13b18
d.alexanov <d.alexanov@gmail.com>
2016-05-30 19:40:38 +0300

    dhilt bot token

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,5 +1,5 @@
 Slack.configure do |config|
-  config.token = 'xoxb-38890365026-9rhS6WkIQ9erY4BnQ5lp6E5S'
+  config.token = 'xoxb-40730681238-fus1sAETbgJF1vKh917uUxJp'
   fail 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 end
 
