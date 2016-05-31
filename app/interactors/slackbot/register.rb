0eb1614e1b1fd606cdaadc67f405c20468a30d07
d.alexanov <d.alexanov@gmail.com>
2016-05-31 17:44:30 +0300

    registration works

--- app/interactors/slackbot/register.rb
+++ app/interactors/slackbot/register.rb
@@ -8,6 +8,11 @@ class Slackbot::Register
       context.client.message channel: context.data.channel, text: text
     end
 
+    user = nil
+    email = nil
+    name = context.data.user
+    pass = context.password
+
     begin
       user = context.webClient.users_info(user: context.data.user)
       email = user['user']['profile']['email']
@@ -16,28 +21,33 @@ class Slackbot::Register
       return false
     end
 
-    if true
-      output "Hi <@#{context.data.user}>!
-Your email is " + email + ".
-You have been registered successfully."
-      #context.user = {}
-    else
-      context.fail!(error: { })
+    dbUser = User.find_by(email: email)
+    if dbUser
+      output "Can't register. You are already registered."
+      return false
     end
+
+    if pass.blank?
+      output "Enter your password."
+      context.readyToPassword = true
+      return
+    end
+
+    if pass.length < 6
+      output "Password must have at least 6 symbols."
+      return
+    end
+
+    dbUser = User.new({:email => email, :password => pass, :password_confirmation => pass})
+    unless dbUser.save
+      output "Can't save a new user to DB."
+      #context.fail!(error: { })
+      return
+    end
+
+    output "You have been registered successfully!"
+    context.saved = true
+    return
+
   end
 end
-
-# U0RG9ABMW
-# <Slack::RealTime::Models::User
-# color="a63024" deleted=false id="U0RG9ABMW" is_admin=false is_bot=false is_owner=false is_primary_owner=false
-# is_restricted=false is_ultra_restricted=false name="ykononov" presence="away"
-# profile=
-#   <Slack::Messages::Message avatar_hash="g24c88210f8a" email="ykononov@hill30.com" fields=nil first_name="Юрий"
-#   image_192="https://secure.gravatar.com/avatar/24c88210f8ab769f33caf96510243f39.jpg?s=192&d=https%3A%2F%2Fa.slack-edge.com%2F7fa9%2Fimg%2Favatars%2Fava_0015-192.png"
-#   image_24="https://secure.gravatar.com/avatar/24c88210f8ab769f33caf96510243f39.jpg?s=24&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-24.png"
-#   image_32="https://secure.gravatar.com/avatar/24c88210f8ab769f33caf96510243f39.jpg?s=32&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-32.png"
-#   image_48="https://secure.gravatar.com/avatar/24c88210f8ab769f33caf96510243f39.jpg?s=48&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-48.png"
-#   image_512="https://secure.gravatar.com/avatar/24c88210f8ab769f33caf96510243f39.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2F7fa9%2Fimg%2Favatars%2Fava_0015-512.png"
-#   image_72="https://secure.gravatar.com/avatar/24c88210f8ab769f33caf96510243f39.jpg?s=72&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-72.png"
-#   last_name="Кононов" real_name="Юрий Кононов" real_name_normalized="Jurij Kononov"> real_name="Юрий Кононов" status=nil team_id="T0QJBTAJ3" tz="Asia/Kuwait"
-#   tz_label="Arabia Standard Time" tz_offset=10800>
--- app/workers/slack_worker.rb
+++ app/workers/slack_worker.rb
@@ -14,7 +14,7 @@ class SlackWorker
     webClient = Slack::Web::Client.new
     scheduler = Rufus::Scheduler.new
 
-    reports = {}
+    users = {}
 
     client.on :hello do
       p "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
@@ -28,55 +28,71 @@ class SlackWorker
     end
 
     client.on :message do |data|
-      unless reports[data.channel]
-        reports[data.channel] = {
+
+      unless users[data.channel]
+        users[data.channel] = {
+          ready_to_password: false,
           started: false,
           current_step: nil,
           report: {}
         }
       end
-      p reports
+
+      p users
+
+      current_user = users[data.channel]
+
       case data.text
         when '-h' then
           client.message channel: data.channel, text: "hill30-standup-bot help:
   -h help
+  -r register
   -s start daily report
   -n next report statement"
         when '-s' then
-          if report = reports[data.channel]
-            report[:started] = true
+          if current_user
+            current_user[:started] = true
             client.message channel: data.channel, text: "Hi <@#{data.user}>! Lets start the standup! Enter -n to start or go to the next step"
           end
         when '-r' then
-          Slackbot::Register.call({ client: client, webClient: webClient, data: data })
+          result = Slackbot::Register.call({ client: client, webClient: webClient, data: data })
+          if result.readyToPassword
+            current_user[:ready_to_password] = true
+          end
         when '-n' then
-          if report = reports[data.channel]
-            if report[:started] # TODO: Chech that daily report already exist
-              case report[:current_step]
+          if current_user
+            if current_user[:started] # TODO: Check that daily report already exist
+              case current_user[:current_step]
                 when nil then
                   client.message channel: data.channel, text: FIRST_STEP
-                  report[:current_step] = FIRST_STEP
+                  current_user[:current_step] = FIRST_STEP
                 when FIRST_STEP then
                   client.message channel: data.channel, text: SECOND_STEP
-                  report[:current_step] = SECOND_STEP
+                  current_user[:current_step] = SECOND_STEP
                 when SECOND_STEP then
                   client.message channel: data.channel, text: THIRD_STEP
-                  report[:current_step] = THIRD_STEP
+                  current_user[:current_step] = THIRD_STEP
                 when THIRD_STEP then
                   client.message channel: data.channel, text: "Thank you!"
                   # TODO: put report into DB here
-                  p report
-                  report[:started] = false
-                  report[:current_step] = nil
-                  report[:report] = {}
+                  p current_user
+                  current_user[:started] = false
+                  current_user[:current_step] = nil
+                  current_user[:report] = {}
               end
             end
           end
         else
-          if report = reports[data.channel]
-            if report[:started] && (current_step = report[:current_step])
-              report[:report][current_step] = [] if report[:report][current_step].nil?
-              report[:report][current_step] << data.text
+          if current_user
+            if current_user[:ready_to_password]
+              result = Slackbot::Register.call({ client: client, webClient: webClient, data: data, password: data.text })
+              if result.saved
+                current_user[:ready_to_password] = false
+              end
+            end
+            if !current_user[:ready_to_password] && current_user[:started] && (current_step = current_user[:current_step])
+              current_user[:report][current_step] = [] if current_user[:report][current_step].nil?
+              current_user[:report][current_step] << data.text
             end
           end
       end
