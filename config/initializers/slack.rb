3066f87cf0a73aa7f076e13c8a9d3414dde88705
d.alexanov <d.alexanov@gmail.com>
2016-05-31 13:09:36 +0300

    slack token via ENV

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,5 +1,6 @@
 Slack.configure do |config|
-  config.token = 'xoxb-46720370647-ZNplrdBKuPDd4xzd2hv5zzQh'
+  config.token = ENV['SLACK_API_TOKEN'] #'xoxb-40730681238-fus1sAETbgJF1vKh917uUxJp'
+  p config.token
   fail 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 
   Slack::RealTime.configure do |config|
