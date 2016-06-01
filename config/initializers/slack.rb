cb8f2f04ed2d3cf4cc0972e6508f4fce9cae9ee5
kivanov <kivanov@hill30.com>
2016-06-01 13:13:18 +0300

    added pg

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,6 +1,5 @@
 Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
-  p config.token
   fail 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 
   Slack::RealTime.configure do |config|
