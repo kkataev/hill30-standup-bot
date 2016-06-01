3ec085f7965386c41e27c2c4ae4660e898a90400
kivanov <kivanov@hill30.com>
2016-06-01 12:54:23 +0300

    show actual data on dashboard, logout link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @reports = DailyReport.select([:user_id, :id, :description, 'MAX(created_at)']).group(:user_id)
+    if params[:date]
+      @date = Date.strptime(params[:date], "%Y-%m-%d")
+    else
+      @date = Date.today
+    end
+    @reports = DailyReport.where(created_at: @date.midnight..@date.end_of_day).select([:user_id, :id, :description, 'MAX(created_at)']).group(:user_id)
   end
 
 end
