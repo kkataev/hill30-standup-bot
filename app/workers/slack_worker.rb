e83d3f15095a5cabffa975ea13637225a4329815
d.alexanov <d.alexanov@gmail.com>
2016-06-03 14:04:33 +0300

    bot workflow refactoring

--- app/workers/slack_worker.rb
+++ app/workers/slack_worker.rb
@@ -5,10 +5,6 @@ class SlackWorker
 
   sidekiq_options queue: "slack"
 
-  FIRST_STEP = 'Completed:'
-  SECOND_STEP = 'Working on:'
-  THIRD_STEP = 'Any problems?'
-
   def perform()
     client = Slack::RealTime::Client.new
     webClient = Slack::Web::Client.new
@@ -23,8 +19,8 @@ class SlackWorker
       #scheduler.cron '20 16 * * 1-5' do
       scheduler.cron '* * * * *' do
         # TODO: send daily reminder here
-        p 'Hello... Rufus'
         p Time.now
+        p 'Hello... Rufus'
       end
     end
 
@@ -41,64 +37,17 @@ class SlackWorker
         }
       end
 
-      current_user = users[data.channel]
-      context = { client: client, webClient: webClient, data: data }
+      context = { client: client, webClient: webClient, data: data, user: users[data.channel] }
 
       case data.text
-        when '-h' then
-          Slackbot::Message.send context, "hill30-standup-bot help:
-  -h help
-  -r register
-  -s start daily report
-  -n next report statement"
-        when '-t'
-          Slackbot::Message.send context, "Test passed."
-        when '-s' then
-          if current_user
-            current_user[:started] = true
-              Slackbot::Message.send context, "Hi <@#{data.user}>! Lets start the standup! Enter -n to start or go to the next step"
-          end
-        when '-r' then
-          if Slackbot::Auth.doRegisterStart context
-            current_user[:ready_to_password] = true
-            Slackbot::Message.send context, "Please enter your password." 
-          end
-        when '-n' then
-          if current_user
-            if current_user[:started] # TODO: Check that daily report already exist
-              case current_user[:current_step]
-                when nil then
-                  Slackbot::Message.send context, FIRST_STEP
-                  current_user[:current_step] = FIRST_STEP
-                when FIRST_STEP then
-                  Slackbot::Message.send context, SECOND_STEP
-                  current_user[:current_step] = SECOND_STEP
-                when SECOND_STEP then
-                  Slackbot::Message.send context, THIRD_STEP
-                  current_user[:current_step] = THIRD_STEP
-                when THIRD_STEP then
-                  if result = Slackbot::Report.save(context, current_user[:report])
-                    p result
-                    current_user[:started] = false
-                    current_user[:current_step] = nil
-                    current_user[:report] = {}
-                  end
-              end
-            end
-          end
-        else
-          if current_user
-            if current_user[:ready_to_password]
-              if Slackbot::Auth.doRegister context, data.text
-                current_user[:ready_to_password] = false
-              end
-            end
-            if !current_user[:ready_to_password] && current_user[:started] && (current_step = current_user[:current_step])
-              current_user[:report][current_step] = [] if current_user[:report][current_step].nil?
-              current_user[:report][current_step] << data.text
-            end
-          end
+        when '-t' then Slackbot::Workflow.doTest context
+        when '-h' then Slackbot::Workflow.doHelp context
+        when '-r' then Slackbot::Workflow.doRegister context
+        when '-s' then Slackbot::Workflow.doStartReport context
+        when '-n' then Slackbot::Workflow.doNextReportStatement context
+        else Slackbot::Workflow.doDefault context
       end
+      
     end
 
     client.on :close do |_data|
