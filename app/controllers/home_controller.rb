552cf71b6a8500026eb74981d3cc42fd6878a75c
d.alexanov <d.alexanov@gmail.com>
2016-06-06 18:23:42 +0300

    Teams are available on reports web-site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,11 @@ class HomeController < ApplicationController
 
   def index
     @date = params[:date].present? ? Date.strptime(params[:date], "%Y-%m-%d") : Date.today
-    @reports = DailyReport
-                .where(created_at: @date.midnight..@date.end_of_day)
+    team = params[:team].present? && params[:team].to_i > 0 ? params[:team].to_i : nil
+    @teams = Team.all
+    @team = team ? @teams.find(team) : nil
+    @reports = @team.blank? ? nil : DailyReport
+                .where(team_id: @team, created_at: @date.midnight..@date.end_of_day)
                 .select("DISTINCT ON(user_id) *")
                 .order("user_id, created_at DESC")
   end
